41d286144c8435edbacd5d222cd4f101c8891ea7
Andrew Kelly <76953256+AndrewKellyDev@users.noreply.github.com>
2022-09-07 16:21:50 +0100

    Update with list

--- Src/Screens/FullScreen.js
+++ Src/Screens/FullScreen.js
@@ -2,28 +2,32 @@ import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView, ImageBackgroun
 import React, { useState, useEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
+import axios from 'react-native-axios'
 
 const FullScreenTab = ({ route, navigation }) => {
 
-    const [sounds, setSounds] = useState([])
+    const [sounds, setSounds] = useState()
 
-    useEffect(() => {
-        fetch('https://audio-streaming-backend-api.herokuapp.com/v1/audio/get-audios', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ folderId: item.folderID })
-        }).then(res => res.json())
-            .then(res => setSounds(res.audios));
-    }, [])
+   
 
     var hour = new Date().getHours();
 
     const { item } = route.params;
-    console.log(item)
-    console.log("Sounds " + sounds)
+
+    const image = item.image
+
+    
+    useEffect(() => {
+        axios.post('https://audio-streaming-backend-api.herokuapp.com/v1/audio/get-audios', {
+            folderId: item.folderID,
+          })
+          .then(function (response) {
+            console.log(response.data);
+            setSounds(response.data)
+          })
+    }, [])
+
+
     return (
         <>
             <Image source={item.image} className='w-full h-[40%] no-repeat z-0 absolute' />
@@ -51,13 +55,13 @@ const FullScreenTab = ({ route, navigation }) => {
                 <View className='flex-1 bg-[#121212] h-[100%] w-[100%] p-5'>
                     <Text className='text-white text-xl mb-4 font-["Montserrat"] tracking-tighter'>Sounds</Text>
 
-                    {!sounds ? sounds.map((item, index) => {
+                    {sounds ? sounds.audios.map((item, index) => {
                         return (
                             <>
-                                <View className='flex bg-[#2d2d32] w-[100%] h-20 rounded-lg p-5'>
+                                <View className='flex bg-[#2d2d32] w-[100%] h-20 rounded-lg p-5 mb-4'>
                                     <View className='flex-1 flex-row justify-between items-center'>
                                         <Text className='text-white/70 font-["Montserrat"] tracking-tighter'>{index}</Text>
-                                        <Image source={item.image} className='w-12 h-12 rounded-lg'></Image>
+                                        <Image source={image} className='w-12 h-12 rounded-lg'></Image>
                                         <Text className='text-white font-bold font-["Montserrat"] tracking-tighter'>{item.name}</Text>
                                         <Text className='text-white/70 font-["Montserrat"] tracking-tighter'>10:30</Text>
                                         <View className='w-12 h-12 bg-black/30 rounded-lg justify-center items-center'>
