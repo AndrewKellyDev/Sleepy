acbe2be47e5f304f85b0d0d2dde5bd1d75f1554f
Andrew Kelly <76953256+AndrewKellyDev@users.noreply.github.com>
2022-09-07 11:34:38 +0100

    Update

--- Src/Data/Categories_Data.js
+++ Src/Data/Categories_Data.js
@@ -1,6 +1,7 @@
 const categoriesData = [
     {
         "image": require('../../assets/water.jpg'),
+        "folderID": '12f5WXF1K_ZJzzA2zZhiZn_O_QkV4Tpk4',
         "name": 'Waterfalls & Rain',
         "desc": '5+ Free Sounds',
         "FullDesc": 'Dive into the world of waterfalls and rain sounds as you sleep. Listening to rain is best way to enter to relax your body and enter rem sleep giving your body the best chance for relaxation and recovery',
@@ -9,6 +10,7 @@ const categoriesData = [
     },
     {
         "image": require('../../assets/fire.jpg'),
+        "folderID": '1JoFNbhhF8P7DVJpAjTgUSTsLlDL4aAWv',
         "name": 'Fire',
         "desc": '10+ Free Sounds',
         "colour": 'bg-[#FFD35C]',
